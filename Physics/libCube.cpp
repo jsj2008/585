06e9834c38f30bfcfa4c59ede446c2f8766dca15
Ori <ocohen@ucalgary.ca>
2011-01-17 11:25:52 -0700

    messing with CMake

--- Physics/libCube.cpp
+++ Physics/libCube.cpp
@@ -1,8 +1,9 @@
 #include "libCube.h"
-
+#include <cstdlib>
 libCube::libCube()
 {
 	mass = 1.0;	//1kg
 	fallInertia = new btVector3(0,0,0);
+	int a = rand() % 2;
 	shape = new btBoxShape(btVector3(0.15,0.15,0.15));
 }
